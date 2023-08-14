b09a01002a775ba5290f9936b31df9c5a82502de
Pavlo Korolov <bibonga54@gmail.com>
2023-08-14 23:56:28 +0200

    Ui and core logic improvements

--- src/game.py
+++ src/game.py
@@ -3,6 +3,7 @@ import time
 import platform
 from asciimatics.screen import Screen
 from tetrominoes import TETROMINOES
+from game_interface import draw_help_section, draw_next_section, draw_end_screen
 
 
 # Global variables
@@ -10,14 +11,23 @@ from tetrominoes import TETROMINOES
 board = None
 board_width = 10
 board_height = 20
-previous_tetromino = None
+
 current_tetromino = None
+current_tetromino_type = None
+next_tetromino = None
+next_tetromino_type = None
+
 tetromino_x = None
 tetromino_y = None
-score = None
+
+score = 0
+lines = 0
+
 collision_time = None
+
 current_state = None
 is_paused = False
+is_finished = False
 
 
 class GameState:
@@ -28,8 +38,8 @@ class GameState:
 def choose_tetromino(previous_tetromino):
     available_tetrominos = [tetromino_type for tetromino_type in TETROMINOES.keys(
     ) if tetromino_type != previous_tetromino]
-    random_tetromino_type = random.choice(available_tetrominos)
-    return TETROMINOES[random_tetromino_type][0]
+    next_tetromino_type = random.choice(available_tetrominos)
+    return TETROMINOES[next_tetromino_type][0], next_tetromino_type
 
 
 def draw_tetromino(screen, tetromino, x: int, y: int):
@@ -65,39 +75,42 @@ def update_board(board, tetromino, x: int, y: int):
 
 
 def draw_board(screen, board, board_width, board_height):
+    screen.print_at('  __________________', 0, 0)
+    screen.print_at(' /! +++ Tetris +++ !\ ', 0, 1)
     for row in range(board_height + 2):
-        screen.print_at('!', 0, row)
-        screen.print_at('!', board_width * 2 + 1, row)
+        screen.print_at('!', 0, row + 2)
+        screen.print_at('!', board_width * 2 + 1, row + 2)
         for col in range(board_width * 2 + 2):
-            screen.print_at('=', col, 0)
-            screen.print_at('=', col, board_height + 1)
+            screen.print_at('=', col, 2)
+            screen.print_at('=', col, board_height + 3)
 
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == 0:
                     if col % 2:
-                        screen.print_at('.', col + 1, row + 1)
+                        screen.print_at('.', col + 1, row + 3)
                     else:
-                        screen.print_at(' ', col + 1, row + 1)
+                        screen.print_at(' ', col + 1, row + 3)
                 else:
-                    screen.print_at('[]', col, row + 1)
+                    screen.print_at('[]', col, row + 3)
 
 
-def check_lines_and_score(board, score):
-    amount = 0
-    lines = 0
+def check_lines_and_score(board):
+    global score
+    global lines
+    current_lines = 0
+    amount_of_ones = 0
     for row in range(len(board)):
         for col in range(len(board[0])):
-            amount += board[row][col]
-            if amount == 10:
+            amount_of_ones += board[row][col]
+            if amount_of_ones == 10:
                 remove_line(board, row)
-                lines += 1
-        amount = 0
+                current_lines += 1
+        amount_of_ones = 0
 
-    if lines != 0:
-        score += 100 * lines
-
-    return score
+    if current_lines != 0:
+        score += 100 * current_lines
+        lines += current_lines
 
 
 def remove_line(board, targ_row):
@@ -108,38 +121,61 @@ def remove_line(board, targ_row):
 
 def restart_game():
     global board
-    global previous_tetromino
     global current_tetromino
+    global current_tetromino_type
+    global next_tetromino
+    global next_tetromino_type
     global tetromino_x
     global tetromino_y
     global score
+    global lines
     global collision_time
     global current_state
     global is_paused
+    global is_finished
 
     board = [[0 for _ in range(board_width * 2)] for _ in range(board_height)]
-    previous_tetromino = None
-    current_tetromino = choose_tetromino(previous_tetromino)
+    current_tetromino, current_tetromino_type = choose_tetromino(None)
+    next_tetromino, next_tetromino_type = choose_tetromino(
+        current_tetromino_type)
     tetromino_x = board_width // 2 + 2
     tetromino_y = 0
     score = 0
+    lines = 0
     collision_time = None
     is_paused = False
+    is_finished = False
     current_state = GameState.PLAYING
+
+
+def end_game(screen):
+    global current_state
+    while True:
+        key = screen.get_key()
+        if key == ord('R') or key == ord('r'):
+            screen.clear()
+            current_state = GameState.PLAYING
+            restart_game()
+            break
+        elif key == Screen.KEY_ESCAPE or key == ord('Q') or key == ord('q'):
+            current_state = GameState.GAME_OVER
+            break
 
 
 def display_game(screen):
     global board
-    global previous_tetromino
     global current_tetromino
+    global current_tetromino_type
+    global next_tetromino
+    global next_tetromino_type
     global tetromino_x
     global tetromino_y
     global score
+    global lines
     global collision_time
     global current_state
     global is_paused
-
-    current_platform = platform.system()
+    global is_finished
 
     board = [[0 for _ in range(board_width * 2)] for _ in range(board_height)]
 
@@ -148,20 +184,11 @@ def display_game(screen):
     previous_time = int(time.time() * 1000)
     total_time = 0
 
-    current_tetromino = choose_tetromino(previous_tetromino)
+    current_tetromino, current_tetromino_type = choose_tetromino(None)
+    next_tetromino, next_tetromino_type = choose_tetromino(
+        current_tetromino_type)
     tetromino_x = board_width // 2 + 2
     tetromino_y = 0
-
-    score = 0
-
-    screen.print_at("ASCII Tetris", 0, 0)
-
-    if current_platform == "Windows":
-        screen.print_at("(Windows)", 0, 1)
-    else:
-        screen.print_at("(Unix)", 0, 1)
-
-    screen.refresh()
 
     current_state = GameState.PLAYING
 
@@ -175,20 +202,20 @@ def display_game(screen):
             current_tetromino_speed = 0.025
             key = screen.get_key()
             if not is_paused:
-                if key == Screen.KEY_LEFT:
+                if key == Screen.KEY_LEFT or key == ord('A') or key == ord('a') or key == ord('H') or key == ord('h'):
                     if not check_collision(board, current_tetromino, tetromino_x - 2, tetromino_y):
                         tetromino_x -= 2
 
-                elif key == Screen.KEY_RIGHT:
+                elif key == Screen.KEY_RIGHT or key == ord('D') or key == ord('d') or key == ord('L') or key == ord('l'):
                     if not check_collision(board, current_tetromino, tetromino_x + 2, tetromino_y):
                         tetromino_x += 2
 
-                elif key == Screen.KEY_UP:
+                elif key == Screen.KEY_UP or key == ord('W') or key == ord('w') or key == ord('K') or key == ord('k'):
                     rotated_tetromino = list(zip(*reversed(current_tetromino)))
                     if not check_collision(board, rotated_tetromino, tetromino_x, int(tetromino_y)):
                         current_tetromino = rotated_tetromino
 
-                elif key == Screen.KEY_DOWN:
+                elif key == Screen.KEY_DOWN or key == ord('S') or key == ord('s') or key == ord('J') or key == ord('j'):
                     if not check_collision(board, current_tetromino, tetromino_x, int(tetromino_y) + 1):
                         tetromino_y += 1
 
@@ -204,8 +231,8 @@ def display_game(screen):
                 is_paused = not is_paused
                 continue
 
-            elif key == Screen.KEY_ESCAPE:
-                current_state = GameState.GAME_OVER
+            elif key == Screen.KEY_ESCAPE or key == ord('Q') or key == ord('q'):
+                is_finished = True
                 break
 
             if not is_paused:
@@ -218,11 +245,11 @@ def display_game(screen):
                     elapsed_time = time.time() - collision_time
                     if elapsed_time < 0.5:
                         key = screen.get_key()
-                        if key == Screen.KEY_LEFT:
+                        if key == Screen.KEY_LEFT or key == ord('A') or key == ord('a') or key == ord('H') or key == ord('h'):
                             if not check_collision(board, current_tetromino, tetromino_x - 2, tetromino_y):
                                 tetromino_x -= 2
 
-                        elif key == Screen.KEY_RIGHT:
+                        elif key == Screen.KEY_RIGHT or key == ord('D') or key == ord('d') or key == ord('L') or key == ord('l'):
                             if not check_collision(board, current_tetromino, tetromino_x + 2, tetromino_y):
                                 tetromino_x += 2
 
@@ -236,37 +263,38 @@ def display_game(screen):
                                      tetromino_x, tetromino_y)
                         tetromino_x = board_width // 2 + 2
                         tetromino_y = 0
-                        current_tetromino = choose_tetromino(
-                            previous_tetromino)
-                        previous_tetromino = current_tetromino
+                        current_tetromino = next_tetromino
+                        current_tetromino_type = next_tetromino_type
+                        next_tetromino, next_tetromino_type = choose_tetromino(
+                            current_tetromino_type)
                         collision_time = None
 
                         if check_collision(board, current_tetromino, tetromino_x, tetromino_y):
                             if check_collision(board, current_tetromino, tetromino_x, tetromino_y + 1):
-                                current_state = GameState.GAME_OVER
+                                is_finished = True
                                 break
 
-                score = check_lines_and_score(board, score)
+                check_lines_and_score(board)
                 screen.refresh()
                 draw_board(screen, board, board_width, board_height)
                 draw_tetromino(screen, current_tetromino,
-                               tetromino_x, tetromino_y + 1)
+                               tetromino_x, tetromino_y + 3)
+                draw_next_section(screen, next_tetromino,
+                                  board_width, score, lines)
+                draw_help_section(screen, board_width)
 
             else:
                 screen.print_at("GAME PAUSED", board_width //
-                                2, board_height // 2 + 1)
+                                2, board_height // 2 + 2)
 
             total_time -= frametime
-            screen.print_at(f"Score: {score}", 0, board_height + 2)
-
-            screen.refresh()
+            if is_finished:
+                screen.clear()
+                draw_end_screen(screen, board_width, score, lines)
+                screen.refresh()
+                end_game(screen)
 
     screen.clear()
-    screen.print_at("GAME OVER", board_width // 2 + 1, board_height // 2)
-    screen.print_at("Your Score:", board_width // 2, board_height // 2 + 1)
-    screen.print_at(f"{score}", board_width, board_height // 2 + 2)
-    screen.refresh()
-    time.sleep(2)
 
 
 def main():
--- src/game_interface.py
+++ src/game_interface.py
@@ -1,27 +1,52 @@
-def draw_interface():
-    screen =   "  __________________         ________________ \n"
-    screen +=  " /! +++ Tetris +++ !\       /! +++ Next +++ !\ \n"
-    screen +=  "/====================\     /==================\ \n"
-    screen +=  "! . . . .[] . . . . .!     ! . . . . . . . . .! \n"
-    screen +=  "! . . . .[] . . . . .!     ! . . .[][] . . . .! \n"
-    screen +=  "! . . . .[] . . . . .!     ! . . . .[][] . . .! \n"
-    screen +=  "! . . . .[] . . . . .!     ! . . . . . . . . .! \n"
-    screen +=  "! . . . . . . . . . .!     !==================! \n"
-    screen +=  "! . . . . . . . . . .!     ! Score: 9999999   ! \n"
-    screen +=  "! . . . . . . . . . .!     ! Lines: 12        ! \n"
-    screen +=  "! . . . . . . . . . .!     !==================! \n"
-    screen +=  "! . . . . . . . . . .!       ________________ \n"
-    screen +=  "! . . . . . . . . . .!      /! +++ Help +++ !\ \n"
-    screen +=  "! . . . . . . . . . .!     /==================\ \n"
-    screen +=  "! . . . . . . . . . .!     ! ↑, w, k - Rotate ! \n"
-    screen +=  "! . . . . . . . . . .!     ! ←, a, h - Left   ! \n"
-    screen +=  "! . . . . . . . . . .!     ! →, d, l - Right  ! \n"
-    screen +=  "! . . . . . . . . . .!     ! ↓, s, j - Down   ! \n"
-    screen +=  "! . . . . @ . . . . .!     ! Space - Drop     ! \n"
-    screen +=  "! .[] . . @ . . . . .!     ! g - Toggle Ghost ! \n"
-    screen +=  "![][] . . @ . . . . .!     ! p - Pause        ! \n"
-    screen +=  "![][] . . @[][] . . .!     ! r - Restart      ! \n"
-    screen +=  "![][][] .[][][][][][]!     ! q - Quit         ! \n"
-    screen +=  "!====================!     !==================! \n"
-    print(screen)
-    print(screen[112])
+from asciimatics.screen import Screen
+
+
+def draw_next_section(screen, tetromino, board_width, score, lines):
+    screen.print_at('  ________________ ', board_width * 3 - 2, 0)
+    screen.print_at(' /! +++ Next +++ !\ ', board_width * 3 - 2, 1)
+    screen.print_at('==================== ', board_width * 3 - 2, 2)
+    screen.print_at('! . . . . . . . . .! ', board_width * 3 - 2, 3)
+    screen.print_at('! . . . . . . . . .! ', board_width * 3 - 2, 4)
+    screen.print_at('! . . . . . . . . .! ', board_width * 3 - 2, 5)
+    screen.print_at('! . . . . . . . . .! ', board_width * 3 - 2, 6)
+    screen.print_at('!==================! ', board_width * 3 - 2, 7)
+    screen.print_at('!                  ! ', board_width * 3 - 2, 8)
+    screen.print_at('!                  ! ', board_width * 3 - 2, 9)
+    screen.print_at('==================== ', board_width * 3 - 2, 10)
+    screen.print_at(f"Score: {score}", board_width * 3, 8)
+    screen.print_at(f"Lines: {lines}", board_width * 3, 9)
+    for row in range(len(tetromino)):
+        for col in range(len(tetromino[0])):
+            if tetromino[row][col]:
+                screen.print_at('[]', board_width * 3 + 5 + col * 2, row + 4)
+
+
+def draw_help_section(screen, board_width):
+    screen.print_at('  ________________ ', board_width * 3 - 2, 11)
+    screen.print_at(' /! +++ Help +++ !\ ', board_width * 3 - 2, 12)
+    screen.print_at('==================== ', board_width * 3 - 2, 13)
+    screen.print_at('! ↑, w, k - Rotate ! ', board_width * 3 - 2, 14)
+    screen.print_at('! ←, a, h - Left   ! ', board_width * 3 - 2, 15)
+    screen.print_at('! →, d, l - Right  ! ', board_width * 3 - 2, 16)
+    screen.print_at('! ↓, s, j - Down   ! ', board_width * 3 - 2, 17)
+    screen.print_at('! Space - Drop     ! ', board_width * 3 - 2, 18)
+    screen.print_at('! g - Toggle Ghost ! ', board_width * 3 - 2, 19)
+    screen.print_at('! p - Pause        ! ', board_width * 3 - 2, 20)
+    screen.print_at('! r - Restart      ! ', board_width * 3 - 2, 21)
+    screen.print_at('! q - Quit         ! ', board_width * 3 - 2, 22)
+    screen.print_at('==================== ', board_width * 3 - 2, 23)
+
+
+def draw_end_screen(screen, board_width, score, lines):
+    screen.print_at('========================== ', board_width + 1, 7)
+    screen.print_at('!       GAME OVER        ! ', board_width + 1, 8)
+    screen.print_at('!------------------------! ', board_width + 1, 9)
+    screen.print_at('! Your Score:            ! ', board_width + 1, 10)
+    screen.print_at('!------------------------! ', board_width + 1, 11)
+    screen.print_at('! Cleared Lines:         ! ', board_width + 1, 12)
+    screen.print_at('!------------------------! ', board_width + 1, 13)
+    screen.print_at('!      r - Restart       ! ', board_width + 1, 14)
+    screen.print_at('!       q - Quit         ! ', board_width + 1, 15)
+    screen.print_at('==========================', board_width + 1, 16)
+    screen.print_at(f"{score}", board_width * 3 - 5, 10)
+    screen.print_at(f"{lines}", board_width * 3 - 2, 12)
